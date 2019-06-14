6da186047cffdbeec8213af4331c6839e0e5228d
John M De Leon <johnmdeleon894@gmail.com>
2019-06-14 09:51:24 -0500

    added more stuff

---
+++ src/main/java/mysql/MySQLConnection.java
@@ -0,0 +1,38 @@
+package mysql;
+
+
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
+public class MySQLConnection {
+    public static void main(String[] args) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            Config config = new Config();
+            Connection connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUserName(),
+                    config.getPassword()
+            );
+            Statement statement = connection.createStatement();
+
+            String query = "select first_name, last_name, emp_no from employees limit 10";
+            ResultSet rs = statement.executeQuery(query);
+            while(rs.next()){
+                System.out.println(" name = " + rs.getString("first_name")+ " " + rs.getString(
+                        "last_name"));
+                System.out.println("Emp # = " + rs.getInt( "emp_no"));
+            }
+
+
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
