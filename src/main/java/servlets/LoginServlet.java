7603eee8966c720b6044c4eae9e86ecd6522ee16
Mykal Clinard <mykalclinard@gmail.com>
2019-06-20 21:01:26 -0500

    The loginservlet start

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import dao.DaoFactory;
+import dao.User;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String hash = BCrypt.hashpw(password, BCrypt.gensalt());
+        User user = DaoFactory.getUsersDao().findByUsername(username);
+
+        if (user == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+
+        boolean passwordsMatch = BCrypt.checkpw(password, hash);
+//        boolean validAttempt = password.equals(user.getPassword());
+
+        if (passwordsMatch) {
+            request.getSession().setAttribute("user", user);
+            response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
